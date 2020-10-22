c5bf7216bc7c516cd50e68de32f11ddbec6290e6
shane-decatur <sdecatur20@stuy.edu>
2020-10-22 00:59:11 -0400

    added arrToString and arrayDeepToString

--- Demo.java
+++ Demo.java
@@ -1,4 +1,21 @@
 public class Demo {
+  public static String arrToString(int[] arr){
+    String arrTo = "{";
+    for (int i = 0; i <arr.length; i++) {
+      arrTo += arr[i];
+      if (i < arr.length - 1) arrTo += ", ";
+    }
+    return (arrTo + "}");
+  }
+
+  public static String arrayDeepToString(int[][] arr){
+    String arrDeepTo = "{";
+    for (int i = 0; i < arr.length; i++) {
+      arrDeepTo += arrToString(arr[i]) + ", ";
+    }
+    return arrDeepTo + "}";
+  }
+
   public static void printloop(int n) {
     for (int i=1;i<=n;i++) {
       for (int x=n-i;x>=0;x--) {
@@ -7,6 +24,7 @@ public class Demo {
       System.out.println();
     }
   }
+
   public static void main(String[] args) {
     if (args.length != 0) {
       printloop(Integer.parseInt(args[0]));
