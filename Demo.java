d6a6288263da01d87b3d55822c1cdd372a6d7dce
shane-decatur <sdecatur20@stuy.edu>
2020-10-22 01:30:08 -0400

    all methods added

--- Demo.java
+++ Demo.java
@@ -16,6 +16,32 @@ public class Demo {
     return arrDeepTo + "}";
   }
 
+  public static int[][] create2DArray(int rows, int cols, int maxValue){
+    int[][] arr = new int[rows][cols];
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
+        int tempInt = Math.round((int) ((maxValue + 1) * Math.random()));
+        arr[i][j] = tempInt;
+      }
+    }
+    return arr;
+  }
+
+  public static int[][] create2DArrayRandomized(int rows, int cols, int maxValue){
+    int[][] arr = new int[rows][];
+    for (int i = 0; i < rows; i++) {
+      int tempCol = Math.round((int) ((cols + 1) * Math.random()));
+      arr[i] = new int[tempCol];
+    }
+    for (int x = 0; x < rows; x++) {
+      for (int y = 0; y < arr[x].length; y++) {
+        int tempVal = Math.round((int) ((maxValue + 1) * Math.random()));
+        arr[x][y] = tempVal;
+      }
+    }
+    return arr;
+  }
+
   public static void printloop(int n) {
     for (int i=1;i<=n;i++) {
       for (int x=n-i;x>=0;x--) {
